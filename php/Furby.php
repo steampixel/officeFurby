2d0613824a9c620f5716d4041fda160527128966
root <info@steampixel.de>
2016-08-07 17:28:09 +0000

    Changed pathes to python files to relative pathes

--- php/Furby.php
+++ php/Furby.php
@@ -5,7 +5,7 @@ class Furby{
     static function init(){
 
         //init piface board
-        shell_exec('sudo python /var/www/python/init.py');
+        shell_exec('sudo python ../python/init.py');
 
         //force audio jack
         shell_exec('sudo amixer cset numid=3 1');
@@ -23,25 +23,25 @@ class Furby{
     
     public static function startEngine(){
         
-        shell_exec('sudo python /var/www/python/engine_start.py');
+        shell_exec('sudo python ../python/engine_start.py');
         
     }
     
     public function stopEngine(){
         
-        shell_exec('sudo python /var/www/python/engine_stop.py');
+        shell_exec('sudo python ../python/engine_stop.py');
         
     }
 
     public function startAmp(){
         
-        shell_exec('sudo python /var/www/python/amp_start.py');
+        shell_exec('sudo python ../python/amp_start.py');
         
     }
 
     public function stopAmp(){
         
-        shell_exec('sudo python /var/www/python/amp_stop.py');
+        shell_exec('sudo python ../python/amp_stop.py');
         
     }
     
